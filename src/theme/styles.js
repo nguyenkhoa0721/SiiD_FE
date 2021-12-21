95615784bb21734119c9583ad1c5ddd9d352a2cc
nguyenvoanhkhoa1 <nvoanhkhoa@gmail.com>
2021-12-21 16:36:32 +0700

    update

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -5,16 +5,24 @@ export const globalStyles = {
     gray: {
       700: "#1f2733",
     },
+    white: "#FFFFFF",
+    black: "#333333",
+    gray1: "#718096",
+    green1: "#18A558",
+    green2: "#008060",
+    green3: "#116530",
+    gray2: "#F8F9FA",
+    pink: "#E1444D",
   },
   styles: {
     global: (props) => ({
       body: {
         bg: mode("gray.50", "gray.800")(props),
-        fontFamily: 'Helvetica, sans-serif'
+        fontFamily: "Helvetica, sans-serif",
       },
       html: {
-        fontFamily: 'Helvetica, sans-serif'
-      }
+        fontFamily: "Helvetica, sans-serif",
+      },
     }),
   },
 };
--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -33,8 +33,9 @@ import {
 import { IoDocumentsSharp } from "react-icons/io5";
 
 function Profile() {
-  // const textColor = useColorModeValue("gray.700", "white");
-  const textColor = useColorModeValue("white", "white");
+  // const textWhite = useColorModeValue("gray.700", "white");
+  const textWhite = useColorModeValue("white", "black");
+  const textBlack = useColorModeValue("black", "white");
   const bgProfile = useColorModeValue(
     "hsla(0,0%,100%,.8)",
     "linear-gradient(112.83deg, rgba(255, 255, 255, 0.21) 0%, rgba(255, 255, 255, 0) 110.84%)"
@@ -43,13 +44,11 @@ function Profile() {
     "white",
     "rgba(255, 255, 255, 0.31)"
   );
-  // const jobColor = useColorModeValue("gray.400", "gray.300");
-  const jobColor = useColorModeValue("white", "white");
+  const borderAvatar = useColorModeValue("white", "white");
+  const buttonEditColor = useColorModeValue("white", "white");
   return (
     <Flex direction="column">
       <Box
-        // mb={{ sm: "205px", md: "75px", xl: "70px" }}
-        mb="32px"
         borderRadius="15px"
         px="0px"
         display="flex"
@@ -67,89 +66,36 @@ function Profile() {
           position="relative"
           display="flex"
           justifyContent="center"
+          alignItems={{ sm: "center", md: "end" }}
         >
-          {/* <Flex
-            direction={{ sm: "column", md: "row" }}
-            mx="1.5rem"
-            maxH="330px"
-            w={{ sm: "90%", xl: "95%" }}
-            justifyContent={{ sm: "center", md: "space-between" }}
-            align="center"
-            backdropFilter="saturate(200%) blur(50px)"
-            position="absolute"
-            boxShadow="0px 2px 5.5px rgba(0, 0, 0, 0.02)"
-            border="2px solid"
-            borderColor={borderProfileColor}
-            bg={bgProfile}
-            p="24px"
-            borderRadius="20px"
-            transform={{
-              sm: "translateY(45%)",
-              md: "translateY(110%)",
-              lg: "translateY(160%)",
-            }}
-          >
-            <Flex
-              align="center"
-              mb={{ sm: "10px", md: "0px" }}
-              direction={{ sm: "column", md: "row" }}
-              w={{ sm: "100%" }}
-              textAlign={{ sm: "center", md: "start" }}
-            >
-              <Avatar
-                me={{ md: "22px" }}
-                src={avatar}
-                w="80px"
-                h="80px"
-                borderRadius="15px"
-              />
-              <Flex direction="column" maxWidth="100%" my={{ sm: "14px" }}>
-                <Text
-                  fontSize={{ sm: "lg", lg: "xl" }}
-                  color={textColor}
-                  fontWeight="bold"
-                  ms={{ sm: "8px", md: "0px" }}
-                >
-                  Esthera Jackson
-                </Text>
-                <Text
-                  fontSize={{ sm: "sm", md: "md" }}
-                  color={jobColor}
-                  fontWeight="semibold"
-                >
-                  esthera@simmmple.com
-                </Text>
-              </Flex>
-            </Flex>
-          </Flex> */}
           <Flex
             align={{ sm: "center", md: "end" }}
             position={{ sm: "", md: "absolute" }}
             left="32px"
             bottom={"32px"}
-            mt={{ sm: "20px", md: "0px" }}
             direction={{ sm: "column", md: "row" }}
             w={{ sm: "100%" }}
             textAlign={{ sm: "center", md: "end" }}
           >
             <Avatar
               me={{ md: "22px" }}
-              position={{ md: "relative" }}
+              position="relative"
               src={avatar}
               w="200px"
               h="200px"
               borderRadius="full"
               border="4px"
-              borderColor="whiteAlpha.50"
+              borderColor={borderAvatar}
             >
               <Button
                 position="absolute"
-                right="0px"
-                bottom="0px"
-                w="30px"
-                h="30px"
+                right="5px"
+                bottom="5px"
+                w="40px"
+                h="40px"
                 p="0px"
                 borderRadius="full"
+                bgColor={buttonEditColor}
               >
                 <MdEdit color="#18A558" w="20px" h="20px" />
               </Button>
@@ -157,14 +103,14 @@ function Profile() {
             <Flex direction="column" maxWidth="100%">
               <Text
                 fontSize={{ sm: "sm", md: "md" }}
-                color={jobColor}
+                color={textWhite}
                 fontWeight="semibold"
               >
                 Freelacer Designer
               </Text>
               <Text
                 fontSize={{ sm: "xl", md: "2xl", lg: "4xl" }}
-                color={textColor}
+                color={textWhite}
                 fontWeight="bold"
                 // ms={{ sm: "8px", md: "0px" }}
               >
@@ -174,128 +120,174 @@ function Profile() {
           </Flex>
         </Box>
       </Box>
-      {/* <Grid templateColumns={{ sm: "1fr", xl: "repeat(2, 1fr)" }} gap="32px">
-        <Card p="16px" my={{ sm: "24px", xl: "0px" }}>
-          <CardHeader p="12px 5px" mb="12px">
-            <Text fontSize="lg" color={textColor} fontWeight="bold">
-              Profile Information
-            </Text>
-          </CardHeader>
-          <CardBody px="5px">
-            <Flex direction="column">
-              <Text fontSize="md" color="gray.500" fontWeight="400" mb="30px">
-                Hi, I’m Esthera Jackson, Decisions: If you can’t decide, the
-                answer is no. If two equally difficult paths, choose the one
-                more painful in the short term (pain avoidance is creating an
-                illusion of equality).
+      <Grid
+        templateColumns={{ sm: "1fr", xl: "repeat(2, 1fr)" }}
+        gap="32px"
+        mt="32px"
+      >
+        <Card p="32px" boxShadow="0 2px 12px 0 rgb(0 0 0 / 16%)">
+          <Box
+            w="full"
+            h="60px"
+            bgColor="#18A558"
+            borderRadius="15px"
+            boxShadow="0 2px 12px 0 rgb(0 0 0 / 16%)"
+          >
+            <Flex
+              alignItems="center"
+              direction="row"
+              justifyContent="space-between"
+              pl="32px"
+              pr="10px"
+            >
+              <Text fontSize="lg" color={textWhite} fontWeight="bold" my="17px">
+                Your profiles
               </Text>
+              <Button
+                w="40px"
+                h="40px"
+                p="0px"
+                borderRadius="full"
+                bgColor={buttonEditColor}
+              >
+                <MdEdit color="#18A558" w="20px" h="20px" />
+              </Button>
+            </Flex>
+          </Box>
+          <Box
+            w="full"
+            h="full"
+            bgColor="#F8F9FA"
+            borderRadius="15px"
+            mt="32px"
+            p="32px"
+            boxShadow="0 2px 12px 0 rgb(0 0 0 / 16%)"
+          >
+            <Flex
+              alignItems="left"
+              direction="row"
+              justifyContent="space-between"
+              direction="column"
+            >
               <Flex align="center" mb="18px">
                 <Text
                   fontSize="md"
-                  color={textColor}
+                  color={textBlack}
                   fontWeight="bold"
                   me="10px"
                 >
                   Full Name:{" "}
                 </Text>
                 <Text fontSize="md" color="gray.500" fontWeight="400">
-                  Esthera Jackson
+                  Tesa Violet
                 </Text>
               </Flex>
               <Flex align="center" mb="18px">
                 <Text
                   fontSize="md"
-                  color={textColor}
+                  color={textBlack}
                   fontWeight="bold"
                   me="10px"
                 >
-                  Mobile:{" "}
+                  Current job:{" "}
                 </Text>
                 <Text fontSize="md" color="gray.500" fontWeight="400">
-                  (44) 123 1234 123
+                  Freelancer Designer
                 </Text>
               </Flex>
               <Flex align="center" mb="18px">
                 <Text
                   fontSize="md"
-                  color={textColor}
+                  color={textBlack}
+                  fontWeight="bold"
+                  me="10px"
+                >
+                  Country:{" "}
+                </Text>
+                <Text fontSize="md" color="gray.500" fontWeight="400">
+                  Viet Nam
+                </Text>
+              </Flex>
+              <Flex align="center" mb="18px">
+                <Text
+                  fontSize="md"
+                  color={textBlack}
                   fontWeight="bold"
                   me="10px"
                 >
                   Email:{" "}
                 </Text>
                 <Text fontSize="md" color="gray.500" fontWeight="400">
-                  esthera@simmmple.com
+                  tessavio@gmail.com
                 </Text>
               </Flex>
               <Flex align="center" mb="18px">
                 <Text
                   fontSize="md"
-                  color={textColor}
+                  color={textBlack}
                   fontWeight="bold"
                   me="10px"
                 >
-                  Location:{" "}
+                  Phone number:{" "}
                 </Text>
                 <Text fontSize="md" color="gray.500" fontWeight="400">
-                  United States
+                  0123456789
                 </Text>
               </Flex>
-              <Flex align="center" mb="18px">
+              <Flex align="center" mb="18px" alignItems="start">
                 <Text
                   fontSize="md"
-                  color={textColor}
+                  color={textBlack}
                   fontWeight="bold"
                   me="10px"
                 >
-                  Social Media:{" "}
+                  Descriptions:{" "}
                 </Text>
-                <Flex>
-                  <Link
-                    href="#"
-                    color="teal.300"
-                    fontSize="lg"
-                    me="10px"
-                    _hover={{ color: "teal.300" }}
-                  >
-                    <Icon as={FaFacebook} />
-                  </Link>
-                  <Link
-                    href="#"
-                    color="teal.300"
-                    fontSize="lg"
-                    me="10px"
-                    _hover={{ color: "teal.300" }}
-                  >
-                    <Icon as={FaInstagram} />
-                  </Link>
-                  <Link
-                    href="#"
-                    color="teal.300"
-                    fontSize="lg"
-                    me="10px"
-                    _hover={{ color: "teal.300" }}
-                  >
-                    <Icon as={FaTwitter} />
-                  </Link>
-                </Flex>
+                <Text fontSize="md" color="gray.500" fontWeight="400" mb="30px">
+                  Hi, I’m Esthera Jackson, Decisions: If you can’t decide, the
+                  answer is no. If two equally difficult paths, choose the one
+                  more painful in the short term (pain avoidance is creating an
+                  illusion of equality).
+                </Text>
               </Flex>
             </Flex>
-          </CardBody>
+          </Box>
         </Card>
-        <Card p="16px">
-          <CardHeader p="12px 5px" mb="12px">
-            <Text fontSize="lg" color={textColor} fontWeight="bold">
-              Platform Settings
-            </Text>
-          </CardHeader>
-          <CardBody px="5px">
+        <Card p="32px" boxShadow="0 2px 12px 0 rgb(0 0 0 / 16%)">
+          <Box
+            w="full"
+            h="60px"
+            bgColor="#18A558"
+            borderRadius="15px"
+            boxShadow="0 2px 12px 0 rgb(0 0 0 / 16%)"
+          >
+            <Flex
+              alignItems="center"
+              direction="row"
+              justifyContent="space-between"
+              pl="32px"
+              pr="10px"
+            >
+              <Text fontSize="lg" color={textWhite} fontWeight="bold" my="17px">
+                Platform Settings
+              </Text>
+            </Flex>
+          </Box>
+          <Box
+            w="full"
+            h="full"
+            bgColor="#F8F9FA"
+            borderRadius="15px"
+            mt="32px"
+            p="32px"
+            boxShadow="0 2px 12px 0 rgb(0 0 0 / 16%)"
+          >
             <Flex direction="column">
               <Text fontSize="sm" color="gray.500" fontWeight="600" mb="20px">
                 ACCOUNT
               </Text>
               <Flex align="center" mb="20px">
-                <Switch colorScheme="teal" me="10px" />
+                <Switch colorScheme="green" me="10px" />
                 <Text
                   noOfLines={1}
                   fontSize="md"
@@ -306,7 +298,7 @@ function Profile() {
                 </Text>
               </Flex>
               <Flex align="center" mb="20px">
-                <Switch colorScheme="teal" me="10px" />
+                <Switch colorScheme="green" me="10px" />
                 <Text
                   noOfLines={1}
                   fontSize="md"
@@ -317,7 +309,7 @@ function Profile() {
                 </Text>
               </Flex>
               <Flex align="center" mb="20px">
-                <Switch colorScheme="teal" me="10px" />
+                <Switch colorScheme="green" me="10px" />
                 <Text
                   noOfLines={1}
                   fontSize="md"
@@ -336,7 +328,7 @@ function Profile() {
                 APPLICATION
               </Text>
               <Flex align="center" mb="20px">
-                <Switch colorScheme="teal" me="10px" />
+                <Switch colorScheme="green" me="10px" />
                 <Text
                   noOfLines={1}
                   fontSize="md"
@@ -347,7 +339,7 @@ function Profile() {
                 </Text>
               </Flex>
               <Flex align="center" mb="20px">
-                <Switch colorScheme="teal" me="10px" />
+                <Switch colorScheme="green" me="10px" />
                 <Text
                   noOfLines={1}
                   fontSize="md"
@@ -358,7 +350,7 @@ function Profile() {
                 </Text>
               </Flex>
               <Flex align="center" mb="20px">
-                <Switch colorScheme="teal" me="10px" />
+                <Switch colorScheme="green" me="10px" />
                 <Text
                   noOfLines={1}
                   fontSize="md"
@@ -369,9 +361,9 @@ function Profile() {
                 </Text>
               </Flex>
             </Flex>
-          </CardBody>
+          </Box>
         </Card>
-      </Grid> */}
+      </Grid>
     </Flex>
   );
 }
