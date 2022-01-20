3b450e7324a43915ab9ddf20e6c571239381c4a1
lenhatquang97 <lenhatquang97@gmail.com>
2022-01-20 15:11:52 +0700

    Add context

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import AdminLayout from "./layouts/Admin";
 import SignUp from "views/Authentication/SignUp";
 import SignIn from "views/Authentication/SignIn";
+import { AuthenticationProvider } from "store/AuthenticationContext";
 
 ReactDOM.render(
   <HashRouter>
     <Switch>
-      <Route path={`/admin`} component={AdminLayout} />
-      <Route path={`/auth/signup`} exact={true} component={SignUp} />
-      <Route path={`/auth/signin`} exact={true} component={SignIn} />
-      <Redirect from={`/`} to="/admin/dashboard" />
+      <AuthenticationProvider>
+        <Route path={`/admin`} component={AdminLayout} />
+        <Route path={`/auth/signup`} exact={true} component={SignUp} />
+        <Route path={`/auth/signin`} exact={true} component={SignIn} />
+        <Redirect from={`/`} to="/admin/dashboard" />
+      </AuthenticationProvider>
     </Switch>
   </HashRouter>,
   document.getElementById("root")
