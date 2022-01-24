ba31d87378c5313501fbc10446b72b3d16cba83e
nguyenvoanhkhoa1 <nvoanhkhoa@gmail.com>
2022-01-25 01:22:44 +0700

    Change to URL

--- src/utils/path/internalPaths.js
+++ src/utils/path/internalPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://nmcnpm-siid.herokuapp.com/api/v1";
+export const BASE_URL = "http://34.132.24.23/api/v1";
 export const USER_LOGIN = BASE_URL + "/user/login";
 export const USER_SIGNUP = BASE_URL + "/user/signup";
 
